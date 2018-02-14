8dfb36db495cc9aaba490c869e0c6b07ff7a3cb7
fnaiemah <kfatinnaiemah@yahoo.com>
2018-02-14 20:53:52 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Online Shop</title>
+	
+	<link rel="stylesheet" href="styles/styles.css" media="all"/>
+	
+</head>
+<body>
+
+<div class="main_wrapper">
+	
+    <div class="header_wrapper">
+        <img src="images/banner.png"/>
+    <div></div>
+    </div>
+    
+    <div class="content_wrapper">
+        <div id="sidebar">Sidebar</div>
+        <div id="content_area">Content</div>
+    </div>
+    
+    <div id="footer">Footer</div>
+    </div>
+    
+</body>
+</html>
